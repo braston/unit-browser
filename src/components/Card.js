a4b11a7eabec7a110510ed1ea64bf9fba84343d9
Brandon <simonthefrog@hotmail.com>
2021-07-30 09:28:06 -0400

    Updated link for images (to support build environment). Updated jpg -> JPG to fix linking

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ const Card = ({ id, name, cost, attack, defense, move, characteristics, img, car
     if(clicked){
         return(
             <div className='tc dib br3 pa3 ma2 grow bw2 shadow-5 w-20' id={id} style={{background:"#d4d2bc",height:"20em",overflowY:"scroll"}} onClick={cardClick}>
-            <img alt={name} src={img} />
+            <img alt={name} src={process.env.PUBLIC_URL + img} />
             <div>
                 <h3 className='tc'>{name}</h3>
                 <p className='f5'><b>Characteristics: </b>{characteristics}</p>
